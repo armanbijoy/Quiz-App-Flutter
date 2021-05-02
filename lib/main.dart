a64ebc6360069d6208ab31712d7c521bdd5fd03c
Arman Bijoy <armanbijoy4@gmail.com>
2021-05-02 08:46:51 +0600

    All Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+
 void main() {}
 
-class MyApp {}
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello'),
+    );
+  }
+}
